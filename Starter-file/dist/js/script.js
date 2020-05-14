da42cf8055789bf43ce59bc6c5898108839b876d
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2020-05-14 18:06:03 +0000

    Update script.js

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -6,11 +6,16 @@ const questionnes = document.querySelectorAll(".questionnes");
 const next = document.querySelector(".next");
 const precedent = document.querySelector(".precedent");
 const barProgress = document.querySelector(".w3-blue");
-const vraisChoix = document.querySelectorAll(".vrais");
+// const vraisChoix = document.querySelectorAll(".vrais");
 const fauxChoix = document.querySelectorAll(".faux");
 const btnResule = document.querySelector(".rslBtn");
 const questionProgres = document.querySelector(".progres");
-const resultFinal = document.querySelector(".rsltMsg");
+const resultFinal = document.querySelector(".Preambule p");
+// const V1 = document.querySelector(".vrais1");
+// const V2 = document.querySelector(".vrais2");
+// const V3 = document.querySelector(".vrais3");
+// const V4 = document.querySelector(".vrais4");
+// const resultMessage = document.querySelectorAll('.Preambule p')
 // console.log(questionnes);
 
 // console.log(stepers);
@@ -24,150 +29,17 @@ function firstStep() {
     stepers[1].classList.add("shadow");
     preambule.style.display = "none";
     contentQuestion.classList.remove("hide");
-    questionnes.classList.add('remove1');
+    // questionnes.classList.add('remove1');
     // presedentButton();
 }
-// for( i=0; i < questionnes.length;i++ ){
-//     next.addEventListener("click", () => {
-
-//         if (i < questionnes.length) {
-
-//             questionnes[i].classList.remove("remove");
-//             // questionnes[i-1].classList.add("remove");
-//             // next.disabled = true;
-//             // if (i === 21) {
-//             //     nextBtn.innerText = "Terminer le test";
-//             //     nextBtn.classList.add("result");
-//             //     let resultBtn = document.querySelector(".result");
-//             //     resultBtn.addEventListener("click", Results);
-//             // } else {
-
-//             }
-//         });
-
-// }
-
-// const i = 0;
-// do {
-//     next.addEventListener("click", () => {
-//         if (i < questionnes.length) { 
-//             questionnes[i].classList.remove("remove");
-//             // questionnes[i-1].classList.add("remove");
-//             // next.disabled = true;
-//             // if (i === 21) {
-//             //     nextBtn.innerText = "Terminer le test";
-//             //     nextBtn.classList.add("result");
-//             //     let resultBtn = document.querySelector(".result");
-//             //     resultBtn.addEventListener("click", Results);
-//             // } else {
-
-//             }
-//         });
-
-//         i++;
-// } while (i < questionnes.length);
-// function startReponse() {
-//     if(){
-//         for (i = 0; i <= questionnes.length; i++) {
-//             console.log(i);
-//             next.addEventListener("click", startReponse);
-//             questionnes[i].classList.remove("remove");
-//             questionnes[i - 1].classList.add("remove");
-//             console.log([i - 1]);
-//         }
-//     }
-
-// }
-
-// nextBtn.addEventListener("click", () => {
-//     if (currentQuestionIndex < 21) {
-//         currentQuestionIndex++;
-//         showQuestion(questions[currentQuestionIndex]);
-//         folowProgress(currentQuestionIndex);
-//         hideprevious();
-
-//         nextBtn.disabled = true;
-//         if (currentQuestionIndex === 21) {
-//             nextBtn.innerText = "Terminer le test";
-//             nextBtn.classList.add("result");
-//             let resultBtn = document.querySelector(".result");
-//             resultBtn.addEventListener("click", Results);
-//         } else {
-//             nextBtn.innerText = "Suivant";
-//         }
-//     }
-// });
-// next.addEventListener("click", function (event) {
-//     var value = event.target.questionnes;
-//         if ( i = 0 && next.addEventListener('click')) {
-
-//         }
-// });
 
 
 
-//     // questionnes.forEach(element => {
-//     //     i = 0;
-//     //     if (i < questionnes.length) {
-//     //         questionnes[0].classList.remove("remove");
-//     //         i++
-//     //     }
-//     // })
-//     // questionnes.forEach(function(item){
-//     //     console.log(item)
-//     // });
-// }
-
-// questionnes.forEach(element => {
-//     next.onclick = function () {
-//         for (i = 0; i <= questionnes.length; i++) {
-//             console.log(i);
-//             questionnes[i].classList.remove("remove");
-//             // questionnes[i - 1].classList.add("remove");
-//             // console.log([i - 1]);
-
-//         }
-//     }
-// })
-
-// next.addEventListener('click',e =>{
-
-//     if(e.target.classList === 'remove'){
-//        e.target.classList.remove("remove");
-//     //    localStorage[''] = list.innerHTML
-//     }
-
-//  });
-
-// function hdiud() {
-//     for (i = 0; i < questionnes.length; i++) {
-
-//         next.addEventListener("click", () => {
-//             if (i === 0) {
-
-//                 questionnes[i].classList.remove('remove');
-
-//             } else {
-
-//                 questionnes[i].classList.remove('remove');
-//                 questionnes[i - 1].classList.add('remove');
-
-//             }
-//         })
-//     }
-// }
-// function presedentButton(){
-//     if (i === 0) {
-//         precedent.classList.add("remove");
-//     } else {
-//         precedent.classList.remove("remove");
-//     }
-// }
-
-let i = 1;
+let i = 0;
 next.addEventListener("click", () => {
     if (i === 0) {
         questionnes[i].classList.remove('remove');
+        showResult();
     } else if (i === questionnes.length){
         next.style.display="none";
         precedent.style.display="none";
@@ -176,13 +48,14 @@ next.addEventListener("click", () => {
         questionnes[i].classList.remove('remove');
         questionnes[i - 1].classList.add('remove');
         precedent.classList.remove('hide');
-
+        showResult();
     }
    
     i++;
     progress(i-1) 
+   
 })
-console.log(i);
+// console.log(i);
 precedent.addEventListener("click", () => {
         questionnes[i].classList.add('remove');
         questionnes[i - 1].classList.remove('remove');
@@ -196,53 +69,90 @@ precedent.addEventListener("click", () => {
 function progress(number) {
     const numberProgres = number + 1;
     questionProgres.innerText = numberProgres;
-    barProgress.style.width = `calc(${numberProgres} * calc(100% / 5))`;
+    barProgress.style.width = `calc(${numberProgres} * calc(100% / 8))`;
 
 }
 
 
-// function barProgress(number) {
+var all = [];
+var stockResult = [];
+function showResult() {
+    vraisChoix = document.querySelectorAll(".vrais");
+    vraisChoix[i].addEventListener("click", () => {
+        var rslt = all.push("");
+        stockResult.push(rslt);
+        // console.log('jddkjdk')
+        // console.log(i);
+        // console.log(all);
+        // console.log(hdhdhd);
+        // var obj = "ssss";
+        // var newData = [...all, obj];
+        // jjd.push(newData);
+        // console.log(newData);
+        // console.log(jjd);
+        console.log(stockResult);
+        console.log(rslt);
+        // return newData;
+    });
+    
+}
 
-//     const currentNmber = number + 1
-//     barProgress.style.width = `calc(${currentNmber} * calc(100% / 22))`
 
-// }
-// const anserStock = [];
-// vraisChoix.addEventListener('click', () =>{
-//     anserStock.push(vrais)
+btnResule.addEventListener("click", () => {
+    stepers[1].classList.remove("shadow");
+    stepers[2].classList.add("shadow");
+    contentQuestion.classList.add("hide");
+    preambule.style.display = "block";
+    // btnResule.classList.remove('hide');
+    btnResule.style.display = "block";
+    btnResule.textContent =  ' Recommencer le test';
+    btnResule.addEventListener('click', ()=>{
+        window.location.reload();
+    })
+    if(stockResult.length === questionnes.length){
+        resultFinal.innerHTML = 'Nous vous conseillons de rester à votre domicile et de contacter votre médecin' +
+                ' en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouv' +
+                'eau l’application pour réévaluer vos symptômes'
+        
+        
+        // resultFinal.innerHTML = `<div>machi korona</div>`
+    }else if(stockResult.length >= questionnes.length/2){
+        resultFinal.innerHTML = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Pren' +
+        'ez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+    }else if(stockResult.length < questionnes.length/2){
+        resultFinal.innerHTML = 'Votre situation ne relève probablement pas du Covid-19.' +
+        'N’hésitez pas à contacter votre médecin en cas de doute.' + 'Vous pouvez refaire le test en cas de nouveau symptôme pour réévaluer la situation.' + 'Pour toute information concernant le Covid-19 allez vers la page d’accueil.'
+    }
+});
+
+// btnResule.addEventListener('click', ()=>{
+//     window.location.reload();
 // })
+    
+        
+    
 
-// function answer(){
-//     for(i=0 ; i<=anserStock ; i++){
 
-//     }
-// }
-// let ansrrr = [];
 
-// function jikko(){
-//     for(i=0;i<=vraisChoix.length;i++){
-//         vraisChoix[i].addEventListener("click", ()=>{
-//             const Ansewr = ansrrr.push("vrais");
-//         })
-//     }
-// }
+// V2.addEventListener('click', ()=>{
+    
+//         anserStock.push('1');
+    
+// } );
 
-// jikko()
-// const answer = [];
-// let a = 0;
-// vraisChoix[a].addEventListener('click', () => {
-//     // console.log(vraisChoix[a]);
-//     if (a === 0) {
-//         vraisChoix[a].classList.add('yaya');
-//     } else {
-//         vraisChoix[a].classList.add('yaya');
-//     }
-//     // console.log(Answers);
-//     a++;
-//     // vraisChoix[a].classList.add('yaya');
-//     // a++;
-// });
+// V3.addEventListener('click', ()=>{
+    
+//     anserStock.push('vrais');
 
+// } );
+// V4.addEventListener('click', ()=>{
+    
+//     anserStock.push('vrais');
+
+// } );
+// console.log(anserStock);
+
+// 
 // console.log(a);
 
 
@@ -268,11 +178,11 @@ function progress(number) {
 //     vraisChoix[a].classList.add('yaya');
 // }, false);
 
-btnResule.addEventListener("click" , ()=>{
-    stepers[1].classList.remove("shadow");
-    stepers[2].classList.add("shadow");
-        contentQuestion.classList.add("hide");
-        resultFinal.classList.remove("hide");
-        resultFinal.innerHTML=`<div>Appelez le 114 est rester chez vous</div>`
+// btnResule.addEventListener("click" , ()=>{
+//     stepers[1].classList.remove("shadow");
+//     stepers[2].classList.add("shadow");
+//         contentQuestion.classList.add("hide");
+//         resultFinal.classList.remove("hide");
+//         resultFinal.innerHTML=`<div>Appelez le 114 est rester chez vous</div>`
     
-})
+// })
